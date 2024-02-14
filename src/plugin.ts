0d4cd1b28d68367728c666383de2b302c758f797
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2024-02-14 23:15:54 +0900

    ノート中の画像も記事に反映されるようにした (#7)
    また、frontmatterに記事のURLを付与する

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,11 @@
-import { Editor, MarkdownView, Plugin, Notice, requestUrl } from "obsidian";
+import {
+	Editor,
+	MarkdownView,
+	Plugin,
+	Notice,
+	requestUrl,
+	normalizePath,
+} from "obsidian";
 import {
 	HatenaPluginSettings,
 	DEFAULT_SETTINGS,
@@ -6,6 +13,9 @@ import {
 } from "./settings";
 import { getWsseHeader } from "./wsse";
 import * as he from "he";
+import mime from "mime";
+
+const fileNameRegex1 = /(jpe?g|png|gif|svg|bmp|webp)$/i;
 
 export default class HatenaPlugin extends Plugin {
 	settings: HatenaPluginSettings;
@@ -30,28 +40,77 @@ export default class HatenaPlugin extends Plugin {
 					return;
 				}
 
+				const token = await getWsseHeader({
+					username: userId,
+					password: apiKey,
+				});
+
 				const file = view.file;
 				if (!file) {
 					new Notice("File not found");
 					return;
 				}
 
-				const hatenaUri =
-					view.app.metadataCache.getFileCache(file)?.frontmatter?.[
-						"hatena-uri"
-					];
-				const { url, method } = hatenaUri
-					? {
-							url: hatenaUri,
-							method: "PUT",
-					  }
-					: {
-							url: `${rootEndpoint}/entry`,
-							method: "POST",
-					  };
+				new Notice("Publishing...");
 
-				const title = file.name.replace(/\.md$/, "");
-				let text = editor.getDoc().getValue();
+				const domParser = new DOMParser();
+				let text = editor.getValue();
+
+				const meta = view.app.metadataCache.getFileCache(file);
+
+				if (meta?.embeds) {
+					for (const embed of meta.embeds) {
+						const { original, link } = embed;
+						if (!fileNameRegex1.test(link)) {
+							continue;
+						}
+						const source = view.app.metadataCache.getFirstLinkpathDest(
+							link,
+							file.path,
+						);
+						if (!source) {
+							continue;
+						}
+
+						// Base64 encode the file
+						const fileBinaly = await view.app.vault.adapter.readBinary(
+							normalizePath(source.path),
+						);
+						const base64File = Buffer.from(fileBinaly).toString("base64");
+						const fileMime = mime.getType(source.extension);
+
+						const postUrl = "https://f.hatena.ne.jp/atom/post";
+						const body = `<entry xmlns="http://purl.org/atom/ns#">
+						<dc:subject>Hatena Blog</dc:subject>
+						<title>${he.escape(source.basename)}</title>
+						<content mode="base64" type="${fileMime}">${base64File}</content>
+						</entry>`;
+						const response = await requestUrl({
+							url: postUrl,
+							method: "POST",
+							contentType: "application/xml",
+							headers: {
+								"X-WSSE": token,
+								Accept:
+									"application/x.atom+xml, application/xml, text/xml, */*",
+							},
+							body,
+						});
+						if (response.status !== 201 && response.status !== 200) {
+							new Notice("Failed to upload image");
+							return;
+						}
+						// Get the image id
+						const xmlDoc = domParser.parseFromString(response.text, "text/xml");
+						const imageId =
+							xmlDoc.getElementsByTagName("hatena:syntax")[0].textContent;
+						if (imageId) {
+							const re = new RegExp(escapeRegExp(original), "g");
+							text = text.replace(re, `[${imageId}]`);
+						}
+					}
+				}
+
 				const position =
 					view.app.metadataCache.getFileCache(file)?.frontmatterPosition;
 				if (position) {
@@ -59,8 +118,8 @@ export default class HatenaPlugin extends Plugin {
 					text = text.split("\n").slice(end).join("\n");
 				}
 
-				// Replace internal link: [[text]] -> text
-				text = text.replace(/\[\[(.+?)\]\]/g, "$1");
+				text = replaceInternalLink(text);
+				const title = file.name.replace(/\.md$/, "");
 
 				const body = `<?xml version="1.0" encoding="utf-8"?>
 				<entry xmlns="http://www.w3.org/2005/Atom"
@@ -69,10 +128,19 @@ export default class HatenaPlugin extends Plugin {
 				  <content type="text/plain">${he.escape(text)}</content>
 				</entry>`;
 
-				const token = await getWsseHeader({
-					username: userId,
-					password: apiKey,
-				});
+				const savedMemberUri =
+					view.app.metadataCache.getFileCache(file)?.frontmatter?.[
+						"hatena-member-uri"
+					];
+				const { url, method } = savedMemberUri
+					? {
+							url: savedMemberUri,
+							method: "PUT",
+					  }
+					: {
+							url: `${rootEndpoint}/entry`,
+							method: "POST",
+					  };
 
 				const response = await requestUrl({
 					url,
@@ -90,11 +158,19 @@ export default class HatenaPlugin extends Plugin {
 				}
 
 				const memberUri = response.headers.location;
-				if (memberUri) {
-					view.app.fileManager.processFrontMatter(file, (fm) => {
-						fm["hatena-uri"] = memberUri;
-					});
-				}
+
+				const xmlDoc = domParser.parseFromString(response.text, "text/xml");
+				const alternateLink = xmlDoc.querySelector('link[rel="alternate"]');
+				const hatenaUrl = alternateLink?.getAttribute("href");
+
+				view.app.fileManager.processFrontMatter(file, (fm) => {
+					if (memberUri) {
+						fm["hatena-member-uri"] = memberUri;
+					}
+					if (hatenaUrl) {
+						fm["hatena-url"] = hatenaUrl;
+					}
+				});
 
 				new Notice("Published successfully!");
 			},
@@ -113,3 +189,14 @@ export default class HatenaPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
+
+/**
+ * [[link]] -> link
+ * @param text
+ * @returns
+ */
+const replaceInternalLink = (text: string) =>
+	text.replace(/\[\[(.+?)\]\]/g, "$1");
+
+const escapeRegExp = (string: string) =>
+	string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
